39e2138d4ed590a6ed452cbaf02093bf661b1dbb
James Wong <jameswongteckhock@gmail.com>
2026-05-23 22:50:27 +0800

    feat: temporarily change top bar name for auto update testing

--- web/components/layout/TopBar.tsx
+++ web/components/layout/TopBar.tsx
@@ -11,7 +11,7 @@ export default function TopBar() {
       {/* Logo + App name */}
       <div className={styles.appName}>
         <div className={styles.logo}>S</div>
-        SPS
+        TEST
         <span className={styles.appSubtitle}>· Study Planner System</span>
       </div>
 
